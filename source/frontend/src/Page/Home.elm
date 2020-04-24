233933b9473d1ce80595a61668a1e99d09ad3870
jasongauvin <contact.jason.gauvin@gmail.com>
2020-04-24 17:28:28 +0200

    Feat (frontend): Add API call to dynamically request for homepage.

--- source/frontend/src/Page/Home.elm
+++ source/frontend/src/Page/Home.elm
@@ -11,56 +11,91 @@ import Html.Events exposing (onInput)
 import Session exposing (Session)
 import Task exposing (Task)
 
+import Browser exposing (sandbox)
+import Http exposing (Error(..))
+import Json.Decode as Decode exposing (Decoder, int, string, field)
+
 -- MODEL
 
 type alias Model =
     { 
         session : Session
         , images: List Image
+        , error : Maybe String
     }
 
 type alias Image =
-    { src : String
+    { id : Int
+    , name : String
     , description : String
+    , image_original_url : String
     }
+
+
+decodeImage : Decoder Image
+decodeImage =
+   Decode.map4 Image
+     (Decode.field "id" Decode.int)
+     (Decode.field "name" Decode.string)
+     (Decode.field "description" Decode.string)
+     (Decode.field "image_original_url" Decode.string)
+
+type Msg
+    = GotImages (Result Http.Error (List Image))
 
 init : Session -> ( Model, Cmd Msg )
 init session =
     (
     { 
         session = session
-        , images = [
-            {src = "https://thebarkingboutique.com/wp-content/uploads/2019/11/image-1.jpg", description = "This is a cute dog."}
-            , {src = "https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg", description = "This is a beautiful cat."}
-            , {src = "https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg", description = "This is a beautiful cat."}
-            , {src = "https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg", description = "This is a beautiful cat."}
-        ]
+        , images = []
+        , error = Nothing
     }
-    , Cmd.none
+    , getImages
     )
+getImages : Cmd Msg
+getImages =
+    Http.get
+        { url = "http://localhost:4000/api/image"
+        , expect = Http.expectJson GotImages (Decode.field "data" (Decode.list decodeImage))
+        }
+
 
 -- VIEW
 
 view : Model -> { title : String, content : Html Msg }
 view model =
     { title = "Home"
-    , content =
-        div [ class "home-page" ]
-            [ div [ class "container page" ]
-                [ 
-                    viewImages model.images
-                ]
+    , content =      
+        let
+            nb_image = List.length model.images
+            
+        in
+        div [ class "images-list" ]
+            [ if nb_image == 0 then
+                text "Loading…"
+
+            else
+                case model.error of
+                    Nothing ->
+                        div []
+                            [ h1 [] [ text (String.fromInt nb_image ++ " images") ]
+                            , List.take 100 model.images
+                                |> List.map viewImage
+                                |> div []
+                            ]
+
+                    Just error ->
+                        div [ ]
+                            [ h1 [] [ text error ] ]
             ]
+
     }
 
-viewImages: List Image -> Html Msg
-viewImages images = 
-    div [ class "images-list" ] (List.map viewImage images)
-
-viewImage: Image -> Html Msg
-viewImage image = 
+viewImage : Image -> Html Msg
+viewImage image =
     div [class "card"] [
-        img [src image.src, class "card-img-top"][]
+        img [src ("http://localhost:4000" ++ image.image_original_url), class "card-img-top"][]
         , div [class "card-body"] [
             p [class "card-text"] [text image.description]
         ]
@@ -68,15 +103,28 @@ viewImage image =
 
 -- UPDATE
 
-
-type Msg = 
-    GotSession Session
-
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotSession session ->
-            ( { model | session = session }, Cmd.none )
+        GotImages (Ok images) ->
+            ( { model
+                | images = images
+                , error = Nothing
+              }
+            , Cmd.none
+            )
+
+        GotImages (Err err) ->
+            let
+                _ =
+                    Debug.log "An error occured" err
+            in
+            ( { model
+                | error = Just <| errorToString err
+                , images = []
+              }
+            , Cmd.none
+            )
 
 
 -- HTTP
@@ -88,15 +136,31 @@ scrollToTop =
         -- If anything, we'd log this to an error recording service.
         |> Task.onError (\_ -> Task.succeed ())
 
+errorToString : Http.Error -> String
+errorToString error =
+    case error of
+        BadUrl url ->
+            "Bad url: " ++ url
+
+        Timeout ->
+            "Request timed out."
+
+        NetworkError ->
+            "Network error. Are you online?"
+
+        BadStatus status_code ->
+            "HTTP error " ++ String.fromInt status_code
+
+        BadBody body ->
+            "Unable to parse response body: " ++ body
+
+
 
 -- SUBSCRIPTIONS
-
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
-
-
 
 -- EXPORT
 
